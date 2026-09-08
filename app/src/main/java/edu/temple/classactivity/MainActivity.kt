6b1886e6a6ce8f20cfa28eb20d482903193029b0
Chaimae Toughzaoui <tuv37170@temple.edu>
2026-09-08 12:25:41 -0400

    add cat Image

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,6 +1,8 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +12,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val imageView : ImageView = findViewById(R.id.imageView)
+
+        var id: Int = R.drawable.
+
+        findViewById<Button>(R.id.button).setOnClickListener{
+            if (id == ) {
+                imageView.setImageResource(R.drawable)
+                id = R.drawable
+            } else {
+                imageView.setImageResource(R.drawable)
+                id = R.drawable
+            }
+        }
     }
 }
